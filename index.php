d2b28bccff7fc1d85d1edd7179def30327423d40
lluczaj <luczaj.lukasz@gmail.com>
2017-01-31 14:33:13 +0100

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
   <title>PHP Test</title>
  </head>
  <body>
- <?php echo '<font size="20"><p>Hello Everyone !</p></font>'; ?> 
+ <?php echo '<font size="20"><b><p>Hello SDI ! ! !</p></b></font>'; ?> 
  </body>
 </html>
