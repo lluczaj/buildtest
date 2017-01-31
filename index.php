a8620b43817f1c2827a6a6501214e8b832b24c38
lluczaj <luczaj.lukasz@gmail.com>
2017-01-31 14:45:48 +0100

    Update index.php

--- index.php
+++ index.php
@@ -6,8 +6,8 @@
  <?php echo '<font size="20"><center><b><p>Hello SDI ! ! !\n\n</p></b></center></font>'; 
   $hostname = getenv('HOSTNAME');
   $build = getenv('OPENSHIFT_BUILD_NAME');
-  echo '<font size="10"><center><b><p>Hostname: $hostname\n </p></b></center></font>';
-  echo '<font size="10"><center><b><p>Build: $build\n </p></b></center></font>';
+  echo '<font size="10"><center><b><p>Hostname: $hostname \n </p></b></center></font>';
+  echo '<font size="10"><center><b><p>Build: $build \n </p></b></center></font>';
   ?> 
  </body>
 </html>
