a7ffa37153ecb864b8371fc2bf9b1c30840f8f1d
LL <lukasz.luczaj@intel.com>
2017-07-31 15:38:55 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Test repository for OpenShift PHP build
---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+ <head>
+  <title>PHP Test</title>
+ </head>
+ <body>
+ <?php echo '<font size="20"><center><b><p>Hello SDI !!!!!</p></b></center></font>'; 
+  $hostname = getenv('HOSTNAME');
+  $build = getenv('OPENSHIFT_BUILD_NAME');
+  echo "<span style='font-size: 15pt'><center><p>Hostname:<b><font color='red'> ".$hostname."</font></b></p></center></span>";
+  echo "<span style='font-size: 15pt'><center><p>Build:<b><font color='red'> ".$build."</font></b></p></center></span>";
+  ?> 
+ </body>
+</html>
