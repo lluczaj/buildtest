ac60f427859ddf79d6f4b23df052a5706e7ddea6
Lukasz <lukasz.luczaj@intel.com>
2017-12-13 15:09:22 +0100

    Update index.php

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
   $build = getenv('OPENSHIFT_BUILD_NAME');
   echo "<span style='font-size: 15pt'><center><p>Hostname:<b><font color='red'> ".$hostname."</font></b></p></center></span>";
   echo "<span style='font-size: 15pt'><center><p>Build:<b><font color='red'> ".$build."</font></b></p></center></span>";
-  for($i = 0; $i < 1000000000; $i++) {
+  for($i = 0; $i < 1000; $i++) {
      $a += $i;
   }
   ?> 
